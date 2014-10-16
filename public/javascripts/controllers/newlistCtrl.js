e5bd1da403b704ea13d1ef3b6aef2034a471e423
EnricAlmiñana <alminyana@gmail.com>
2014-10-16 12:27:39 +0200

    template newlist.html updated css and neslistCtrl.js

--- public/javascripts/controllers/newlistCtrl.js
+++ public/javascripts/controllers/newlistCtrl.js
@@ -1,5 +1,24 @@
 angular
   .module('app')
   .controller('newlistCtrl', ['$scope', function($scope) {
-    $scope.title = "Crear Lista";
+    $scope.unid = 1;
+    $scope.title = "Nueva Lista";
+    var items = [];
+    $scope.items = items;
+    $scope.numeroItems = $scope.items.length;
+    $scope.afegir = function() {
+       $scope.nuevoTitulo = $scope.nombre;
+    };
+    $scope.afegirProd = function() {
+      var objeto = {
+        id: $scope.nombreProd,
+        nombre: $scope.nombreProd,
+        unid: $scope.unid
+      }
+      $scope.items.push(objeto);
+      console.log(items);
+      $scope.nombreProd = '';
+      $scope.unid = 1;
+    };
+
   }]);
