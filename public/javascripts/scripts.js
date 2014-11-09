e566e8bdf57bcf78afc7e59647421da5edebd566
EnricAlmiñana <alminyana@gmail.com>
2014-11-09 18:03:29 +0100

    fixed autoscroll ui.router and edit jquery scripts.js

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,4 +1,24 @@
 $(document).ready(function(){
+  function marcarActivo() {
+    var href = window.location.href;
+    console.log(href);
+    var num = href.lastIndexOf('/');
+    var path = href.substring(num+1);
+    console.log(path);
+    switch (path) {
+      case '':
+        $('#tab li:first').addClass('active');
+        break;
+      case 'newlist':
+        $('#dos').addClass('active');
+        break;
+      case 'todas':
+        $('#tres').addClass('active');
+        break;
+    }
+  }
+  marcarActivo();
+  //$('#tab li:first').addClass('active');
   $('#tab li').click(function(){
     $('#tab li').removeClass('active');
     $(this).addClass('active');
